4baaf61d5e03392c3a72bc89c7deb3affdbf83d5
Micah Rensch <micah.rensch@kellogg.northwestern.edu>
2024-03-01 01:15:53 +0000

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+
+    if @user != nil
+      if @user["password"] == params["password"]
+        flash["notice"] = "Welcome."
+        redirect_to "/places"
+      else 
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
   
