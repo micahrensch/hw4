e5f1fac1d39ff8175bb67d388cb098be5c27e0b2
Micah Rensch <micah.rensch@kellogg.northwestern.edu>
2024-03-10 14:11:36 +0000

    update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -10,8 +10,11 @@ class EntriesController < ApplicationController
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = session["user_id"]
-    @entry.save
+    @entry.uploaded_image.attach(params["entry"]["uploaded_image"]) if params["entry"]
+    if @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
+    else
+    end
   end
 
 end
